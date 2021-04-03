48c1e37f60e6f9f0b21ea2ffeb4f78b9a00471ce
Ghazanfar-ijaz <ghazanfar@outlook.fr>
2021-04-03 17:05:18 +0200

    start

---
+++ main.js
@@ -0,0 +1,3 @@
+const fs = require('fs')
+const readline = require('readline-sync')
+const chalk = require('chalk')
---
+++ wordbank.js
@@ -0,0 +1,2 @@
+const fs = require('fs')
+const { randomInt } = require('crypto')
