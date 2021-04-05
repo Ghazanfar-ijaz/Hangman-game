dd48941c3f46121254ceeef09116d7ebfd43966d
Ghazanfar-ijaz <ghazanfar@outlook.fr>
2021-04-05 21:20:59 +0200

    in progress

--- main.js
+++ main.js
@@ -1,3 +1,59 @@
 const fs = require('fs')
-const readline = require('readline-sync')
+const readlineSync = require('readline-sync')
 const chalk = require('chalk')
+
+
+const words = ['alyra', 'blockchain', 'developpeur', 'bitcoin']
+
+const getRandomWord = (words) => {
+  let randomIndex = Math.floor(Math.random() * words.length)
+  return words[randomIndex]
+}
+
+const CreateHiddenWord = (word) => {
+  let hiddenWord = []
+  for (let i = 0; i < word.length; i++) {
+    hiddenWord.push('_')
+  }
+  return hiddenWord
+}
+
+const isLetterInWord = (letter, word) => {
+  return word.includes(letter)
+}
+
+const replaceletter = (letter, randomWord, hiddenWord) => {
+  for (let i = 0; i < randomWord.length; i++) {
+    let character = randomWord[i]
+    if (character === letter) {
+      hiddenWord[i] = letter;
+    }
+  }
+  return hiddenWord;
+}
+
+
+const startGame = () => {
+  let randomWord = getRandomWord(words);
+  let hiddenWord = CreateHiddenWord(randomWord)
+
+  let tries = 8;
+
+  while (tries > 1 && !(hiddenWord.join('') == randomWord)) {
+    console.log('==============')
+    console.log(`=> Tries: ${tries}`)
+    console.log(randomWord)
+    console.log('=>', hiddenWord.join(''))
+    const answer = readlineSync.question('Type a letter: ')
+
+    if (isLetterInWord(answer, randomWord)) {
+      console.log('Good! that letter is in the word!')
+      hiddenWord = replaceletter(answer, randomWord, hiddenWord);
+    } else {
+      tries--;
+      console.log('Nope! that letter is not in the word!')
+    }
+  }
+
+}
+startGame()
